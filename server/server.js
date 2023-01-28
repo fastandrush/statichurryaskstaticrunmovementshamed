55b15c47876e99c466f6aaf4801dbcb18556033a
fasterrush <fasterrush@gmail.com>
2023-01-28 10:19:04 +0800

    first commit

--- server/server.js
+++ server/server.js
@@ -80,17 +80,22 @@ app.use('/share', shareRoute);
 app.use('/storeditemrevenue', storedItemRevenueRoute);
 app.use('/funds', fundsRoute);
 
-app.use(express.static(__dirname, './view/build'))
+
+app.use(express.static(__dirname, 'view/build'))
+
+//app.use(express.static('public'));
 
 mongodb.log(mongoose.connection);
 console.log(process.env.NODE.ENV)
 
 if ( process.env.NODE.ENV === 'production' ) {
   app.get('*', (req,res) => {
-     res.sendFile(path.join(_dirname, './view/build', 'index.html'));
+     res.sendFile(path.join(_dirname, 'view/build', 'index.html'));
   })
 
-} 
+}   
+
+
 
 console.log(process.env.ATLAS_URI)
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
